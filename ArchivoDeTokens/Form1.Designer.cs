4ce5b89013c06934e17061fb310e797f7a9650e5
jorge <jorge@LAPTOP-2JQLGHQK>
2026-04-16 21:03:44 -0500

    Agregar archivos de proyecto.

---
+++ ArchivoDeTokens/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ArchivoDeTokens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLexico = new System.Windows.Forms.Button();
+            this.rtxtCodigo = new System.Windows.Forms.RichTextBox();
+            this.rtxtTokens = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLexico
+            // 
+            this.btnLexico.Location = new System.Drawing.Point(362, 186);
+            this.btnLexico.Name = "btnLexico";
+            this.btnLexico.Size = new System.Drawing.Size(104, 41);
+            this.btnLexico.TabIndex = 1;
+            this.btnLexico.Text = "button1";
+            this.btnLexico.UseVisualStyleBackColor = true;
+            this.btnLexico.Click += new System.EventHandler(this.btnLexico_Click);
+            // 
+            // rtxtCodigo
+            // 
+            this.rtxtCodigo.Location = new System.Drawing.Point(34, 43);
+            this.rtxtCodigo.Name = "rtxtCodigo";
+            this.rtxtCodigo.Size = new System.Drawing.Size(322, 314);
+            this.rtxtCodigo.TabIndex = 3;
+            this.rtxtCodigo.Text = "";
+            this.rtxtCodigo.WordWrap = false;
+            // 
+            // rtxtTokens
+            // 
+            this.rtxtTokens.Location = new System.Drawing.Point(472, 43);
+            this.rtxtTokens.Name = "rtxtTokens";
+            this.rtxtTokens.Size = new System.Drawing.Size(322, 314);
+            this.rtxtTokens.TabIndex = 4;
+            this.rtxtTokens.Text = "";
+            this.rtxtTokens.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 531);
+            this.Controls.Add(this.rtxtTokens);
+            this.Controls.Add(this.rtxtCodigo);
+            this.Controls.Add(this.btnLexico);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLexico;
+        private System.Windows.Forms.RichTextBox rtxtCodigo;
+        private System.Windows.Forms.RichTextBox rtxtTokens;
+    }
+}
+
---
+++ ArchivoDeTokens/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ArchivoDeTokens
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, Dictionary<string, string>> matrizTransicion = new Dictionary<int, Dictionary<string, string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarMatrizEnMemoria();
+        }
+
+        private void CargarMatrizEnMemoria()
+        {
+            using (SqlConnection con = ConexionBD.ObtenerConexion())
+            {
+                string query = "SELECT * FROM matriz";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int estadoFila = Convert.ToInt32(reader["ESTADO"]);
+
+                            var filaActual = new Dictionary<string, string>();
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                string nombreColumna = reader.GetName(i);
+
+                                if (nombreColumna != "ESTADO")
+                                {
+                                    string valorCelda = reader[i].ToString();
+                                    filaActual[nombreColumna] = valorCelda;
+                                }
+                            }
+
+                            matrizTransicion[estadoFila] = filaActual;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnLexico_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(rtxtCodigo.Text))
+                    throw new Exception("No hay código");
+
+                string[] lineasCodigo = rtxtCodigo.Lines;
+                int estadoActual = 1;
+                char car;
+                string strCar = "";
+                string celdaActual = "";
+                List<string> filasDeTokens = new List<string>();
+                string tokens = "";
+                bool cadAbierta = false;
+                for (int numLinea = 0; numLinea < lineasCodigo.Length; numLinea++)
+                {
+                    string lineaCodActual = lineasCodigo[numLinea];
+                    string tokensDeLinea = "";
+                    string lineaLista = lineaCodActual.TrimEnd();
+                    if (!lineaLista.EndsWith("~"))
+                    {
+                        //ERROR por falta de simbolo ~ al final de la linea
+                    }
+                    lineaCodActual = lineaLista+" ";
+                    cadAbierta = false;
+
+                    for (int numCaracter = 0; numCaracter < lineaCodActual.Length; numCaracter++)
+                    {
+                        car = lineaCodActual[numCaracter];
+                        if (estadoActual == 1 && (car == ' ' || car == '\t' || car == '\r' || car == '\n'))
+                        {
+                            continue; 
+                        }
+                        if(car == '"')
+                        {
+                            cadAbierta = !cadAbierta;
+                        }
+                        
+                        if((car == ' ' || car == '\t') && cadAbierta)
+                        {
+                            numCaracter++;
+                            car = lineaCodActual[numCaracter];
+                        }
+
+                        if ((car == ' ' || car == '\t') && !cadAbierta)
+                        {
+                            strCar = "FDC";
+                        }
+                        else
+                        {
+                            if (char.IsLetter(car))
+                            {
+
+                                strCar = car.ToString();
+                                if (char.IsLower(car))
+                                {
+                                    strCar = car + "1";
+                                }
+                            }else if (char.IsDigit(car))
+                            {
+                                strCar = "_" + car;
+                            }
+                            else
+                            {
+                                strCar = "c" + (int)car;
+                            }
+                        }
+                        if (!matrizTransicion[estadoActual].ContainsKey(strCar))
+                        {
+                            throw new Exception("Símbolo fuera de la matriz de transición");
+                        }
+                        celdaActual = matrizTransicion[estadoActual][strCar];
+                        if(celdaActual != "ERROR" && celdaActual != "ACEPTA")
+                        {
+                            if (matrizTransicion[int.Parse(celdaActual)]["FDC"] == "ERROR")
+                            {
+                                tokensDeLinea += "[ERROR:" + matrizTransicion[int.Parse(celdaActual)]["CAT"] + "] ";
+                                tokens += "[ERROR:" + matrizTransicion[int.Parse(celdaActual)]["CAT"] + "] ";
+
+                                estadoActual = 1;
+
+                            }
+                            else if (matrizTransicion[int.Parse(celdaActual)]["FDC"] == "ACEPTA")
+                            {
+                                tokensDeLinea += matrizTransicion[int.Parse(celdaActual)]["CAT"] + " ";
+                                tokens += matrizTransicion[int.Parse(celdaActual)]["CAT"]+" ";
+                                estadoActual = 1;
+
+                                if (strCar != "FDC")
+                                {
+                                    numCaracter--;
+                                }
+                            }
+                            else
+                            {
+                                estadoActual = int.Parse(celdaActual);
+                            }
+                        }
+                        
+                    }
+                    tokens.TrimEnd(' ');
+                    tokens += "\n";
+                    filasDeTokens.Add(tokensDeLinea.TrimEnd());
+                }
+                rtxtTokens.Lines = filasDeTokens.ToArray();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
